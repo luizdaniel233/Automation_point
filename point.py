9b1b0d740014a41b4abd54bf5cf1cbccafa6fed0
Luiz Daniel Marinho dos Santos <62406091+luizdaniel233@users.noreply.github.com>
2022-07-24 22:30:59 -0400

    Automation ready

--- point.py
+++ point.py
@@ -13,11 +13,11 @@ class automation_point:
     def automation(self):
 
         nav = webdriver.Chrome(ChromeDriverManager().install())
-        nav.get("https://cliente.apdata.com.br/conecthus/")
+        nav.get("****")
 
         WebDriverWait(nav,30).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[4]/div[4]/div/div/a'))).click()
-        WebDriverWait(nav,30).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[1]/div/div[3]/div/div/div/div/div[2]/div/div/div/div/div[3]/div[4]/input'))).send_keys("600744")
-        WebDriverWait(nav,30).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[1]/div/div[3]/div/div/div/div/div[2]/div/div/div/div/div[3]/div[5]/input'))).send_keys("Luiz@1234")
+        WebDriverWait(nav,30).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[1]/div/div[3]/div/div/div/div/div[2]/div/div/div/div/div[3]/div[4]/input'))).send_keys("***")
+        WebDriverWait(nav,30).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[1]/div/div[3]/div/div/div/div/div[2]/div/div/div/div/div[3]/div[5]/input'))).send_keys("****")
         WebDriverWait(nav,30).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[1]/div/div[3]/div/div/div/div/div[2]/div/div/div/div/input[1]'))).click()
         date = datetime.now()
         msg = f'Automation Sucessfull,point ready at {date}'
